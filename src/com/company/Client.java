583f216728708a5d60d446e65245bb740f24282d
Gurv33r <gurveersingh477@gmail.com>
2021-12-03 19:47:18 -0800

    done

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -4,8 +4,6 @@ import java.net.*;
 import java.nio.channels.*;
 import java.util.Scanner;
 
-
-//simulates client.
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,19 +13,20 @@ public class Client {
         File file = new File("local/" + requestedFile); // make file object
         try {
             URL url = new URL("http://localhost:" + PORT + "/" + requestedFile);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             System.out.println("Sending GET request to " + url);
             // send request and open response
-            ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+            ReadableByteChannel byteChannel = Channels.newChannel(conn.getInputStream());
+            System.out.println("Response Received: " + conn.getResponseMessage()); // print response header
             //prepare file to save to
             file.getParentFile().mkdir(); // make local directory if it doesn't exist
-            file.createNewFile(); // make
-            //create writer
-            System.out.println("Response Received:");
+            file.createNewFile(); // make file
             // setup file saver
             FileOutputStream fileOutputStream = new FileOutputStream(file.getPath());
             FileChannel fileChannel = fileOutputStream.getChannel();
             // transfer file info from response
             fileChannel.transferFrom(byteChannel, 0, Long.MAX_VALUE);
+            System.out.println("Saved to " + file.getCanonicalPath()); // print save location
         } catch(Exception e){
             e.printStackTrace();
         }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 public class Server{
     public static void main(String[] args) throws IOException {
         ServerSocket serversocket = new ServerSocket(80); // establish HTTP receiver socket
-        System.out.println("Server running " + InetAddress.getLocalHost() + ":" + serversocket.getLocalPort());
+        System.out.println("Server running on " + InetAddress.getLocalHost() + ":" + serversocket.getLocalPort());
         final String CRLF = "\r\n";
         try {
             // boot up server
@@ -30,31 +30,23 @@ public class Server{
             PrintWriter writer = new PrintWriter(out, true);
             File file = new File(path); //access file
             if (!file.exists()) { // if requested file doesn't exist, response with 404
-                response = "HTTP/1.1 404 Not Found ";
+                response = "HTTP/1.1 404 Not Found";
                 writer.write(response); //send code
                 // end response
                 writer.append(CRLF).flush();
-                out.flush();
             } else { // file exists, respond with 200 and file requested
                 response = "HTTP/1.1 200 OK ";
                 writer.write(response); // send code
-                // create headers
-                writer.append("Content-Disposition: form-data; name=\"binaryFile\"; filename=\"")
+                // create header
+                writer.append("Content-Disposition: attachment; filename=\"")
                         .append(file.getName())
                         .append("\"")
                         .append(CRLF);
-                String contentType = URLConnection.guessContentTypeFromName(file.getName());
-                writer.append("Content-Type: ")
-                        .append(contentType)
-                        .append(";charset=utf-8")
-                        .append(CRLF);
-                writer.append("Content-Transfer-Encoding: binary")
-                        .append(CRLF);
+                writer.append(CRLF).flush(); // send header
+                Files.copy(file.toPath(), out); // send file
                 writer.append(CRLF).flush();
-                Files.copy(file.toPath(), out); //send file
-                writer.append(CRLF).flush();
-                out.flush();
             }
+            out.flush();
             // close all open readers and writers
             writer.close();
             out.close();
